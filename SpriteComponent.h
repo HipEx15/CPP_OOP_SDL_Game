6646b2cfc9d39c9418192f241aab0876b8da4d63
Alex <86966930+HipEx15@users.noreply.github.com>
2022-01-19 22:25:23 +0200

    Clasa destinata realizarilor de animatii, aceasta se imbina cu clasa Animation.

---
+++ SpriteComponent.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "Components.h"
+#include "Texture.h"
+#include <SDL.h>
+#include "Animation.h"
+#include <map>
+#include "AssetManager.h"
+
+class SpriteComponent : public Component
+{
+private:
+	TransformComponent* transform;
+	SDL_Texture* texture;
+	SDL_Rect srcRect, destRect;
+
+	bool animated = false;
+	int frames = 0;
+	int speed = 100;
+
+public:
+
+	int animIndex = 0;
+
+	std::map<const char*, Animation> animations;
+
+	SDL_RendererFlip spriteFlip = SDL_FLIP_NONE;
+
+	SpriteComponent() = default;
+
+	SpriteComponent(const char* path)
+	{
+		setTex(path);
+	}
+
+	SpriteComponent(std::string id, bool isAnimated)
+	{
+		animated = isAnimated;
+
+		Animation idle = Animation(0, 1, 100);
+		Animation walk = Animation(1, 2, 100);
+
+		animations.emplace("Idle", idle);
+		animations.emplace("Walk", walk);
+
+		Play("Idle");
+
+		setTex(id);
+	}
+
+	~SpriteComponent()
+	{}
+
+	void setTex(std::string id)
+	{
+		texture = Game::assets->GetTexture(id);
+	}
+	
+	void init() override 
+	{
+
+		transform = &entity->getComponent<TransformComponent>();
+
+		srcRect.x = srcRect.y = 0;
+		srcRect.w = transform->width;
+		srcRect.h = transform->height;
+
+	}
+
+	void update() override
+	{
+		if (animated)
+		{
+			srcRect.x = srcRect.w * static_cast<int>((SDL_GetTicks() / speed) % frames);
+		}
+
+		srcRect.y = animIndex * transform->height;
+
+		destRect.x = static_cast<int>(transform->position.x);
+		destRect.y = static_cast<int>(transform->position.y);
+		destRect.w = transform->width / transform->scale;
+		destRect.h = transform->height / transform->scale;
+	}
+
+	void draw() override
+	{
+		TextureManager::Draw(texture, srcRect, destRect, spriteFlip);
+	}
+
+	void Play(const char* animName)
+	{
+		frames = animations[animName].frames;
+		animIndex = animations[animName].index;
+		speed = animations[animName].speed;
+	}
+
+	SDL_Texture* RetTex(void)
+	{
+		return texture;
+	}
+
+	void setFrame(int x, int y)
+	{
+		srcRect.x = x * 64;
+		srcRect.y = y * 64;
+	}
+
+};
